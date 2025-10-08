cfd2dbc603624c82244ba3fd3e486550f8a02e97
rahelaqambari <rahelaqambari@gmail.com>
2025-10-08 17:10:07 +0430

    hi

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,11 +1,7 @@
 import Cart from "@/mycomponents/Cart";
-import Search from "@/mycomponents/Searh";
-import Servicshero from "@/mycomponents/Servicshero";
 export default function Services (){
     return(
         <div>
-            <Servicshero />
-            <Search />
             <Cart />
             
         </div>
--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -1,11 +1,9 @@
 import Clock from "@/mycomponents/Clock";
-import LoginPage from "@/mycomponents/LoginPage";
 
 export default function Weather (){
     return(
         <div>
             <Clock />
-            <LoginPage />
         </div>
     )
 }
